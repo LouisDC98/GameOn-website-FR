5942d46c47d8cb2ebb3b8ac7d529150f7b4ea356
LouisDC98 <79375896+LouisDC98@users.noreply.github.com>
2021-05-12 10:32:23 +0200

    Fix/4/ajouter message validation (#5)
    * Update validateLocation and isValid functions
    
    * Update closeModal function
    
    * Update updateErrorMessage function
    
    * Add validationModal function
    
    * Update code

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -13,77 +13,85 @@ function editNav() {
 
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-const closeModalBtn = document.getElementById('close');   // Define X button as 'close' 
+// Define X button as 'close' 
+const closeModalBtn = document.getElementById('close');
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", closeModal));
-closeModalBtn.addEventListener('click', closeModal);      // Call function closeModal when X pressed
+modalBtn.forEach((btn) => btn.addEventListener("click", displayMainModal));
+
+// Call function closeModal when X pressed
+closeModalBtn.addEventListener('click', displayMainModal);
 
 /*****************Add or remove the bground class*****************/
-function closeModal () {
-  modalbg.classList.toggle('bground--visible');
+function displayMainModal() {
+  if (modalbg.classList.contains('bground--visible')) {
+    modalbg.classList.remove('bground--visible');
+  }
+  else {
+    modalbg.classList.add('bground--visible');
+  }
 }
 
 /****************************************************************/
 /********************Validate or not the form********************/
 /****************************************************************/
 
-const form = document.getElementById('form') ;                 // Define form as 'form'
-const firstNameField = document.getElementById('first') ;      // Define input first as 'firstNameField'
-const lastNameField = document.getElementById('last') ;        // Define input last as 'lastNameField'
-const emailField = document.getElementById('email');           // Define input email as 'email'
-const birthDateField = document.getElementById('birthdate');   // Define birthdate as 'birthdate'
-const tournoisField = document.getElementById('quantity');     // Define input quantity as 'tournois'
-const location1 = document.getElementById('location1') ;       // Define radio 1 as 'location1'
-const location2 = document.getElementById('location2');        // Define radio 2 as 'location2'
-const location3 = document.getElementById('location3');        // Define radio 3 as 'location3'
-const location4 = document.getElementById('location4');        // Define radio 4 as 'location4'
-const location5 = document.getElementById('location5');        // Define radio 5 as 'location5'
-const location6 = document.getElementById('location6');        // Define radio 6 as 'location6'
-const locationField = document.getElementById('location');  
-const checkbox1 = document.getElementById('checkbox1');        // Define checkbox 1 as 'checkbox1'
-const checkboxField = document.getElementById('checkbox');  
-
+// Define all const used for validate each field
+const form = document.getElementById('form');
+const firstNameField = document.getElementById('first');
+const lastNameField = document.getElementById('last');
+const emailField = document.getElementById('email');
+const birthDateField = document.getElementById('birthdate');
+const tournoisField = document.getElementById('quantity');
+const locationField = document.getElementById('location');
+const checkbox1 = document.getElementById('checkbox1');
+const checkboxField = document.getElementById('checkbox');
 
 // Regex which accept mail format
-const formatMail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)+$/; 
+const formatMail = /^[^@]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)+$/;
 // Regex with only numbers
-const formatNumbers = /^[0-9]+$/;  
+const formatNumbers = /^[0-9]+$/;
 // Regex with numbers and -
-const formatDate = /[0-9]\-/;                               
+const formatDate = /[0-9]\-/;
 
 /**************Variables for add or remove error class****************/
-let errorFirst = document.getElementById ('messageFirst');
-let errorLast = document.getElementById ('messageLast');
-let errorEmail = document.getElementById ('messageEmail');
-let errorBirthdate = document.getElementById ('messageBirthdate');
-let errorTournois = document.getElementById ('messageTournois');
-let errorLocation = document.getElementById ('messageLocation');
-let errorCheckbox = document.getElementById ('messageCheckbox');
+let errorFirst = document.getElementById('messageFirst');
+let errorLast = document.getElementById('messageLast');
+let errorEmail = document.getElementById('messageEmail');
+let errorBirthdate = document.getElementById('messageBirthdate');
+let errorTournois = document.getElementById('messageTournois');
+let errorLocation = document.getElementById('messageLocation');
+let errorCheckbox = document.getElementById('messageCheckbox');
 
- function updateErrorMessage(valueField, errorField, isValid) {
-   if (isValid) {
-     errorField.classList.remove('errors--off');
-     valueField.classList.remove('text-control--off');
-   }
-   else {
-    if (!errorField.classList.contains('errors--off')){
+function updateErrorMessage(valueField, errorField, isValid) {
+  if (isValid) {
+    if (errorField.classList.contains('errors--off')) {
+      errorField.classList.remove('errors--off');
+    }
+    if (valueField.classList.contains('text-control--off')) {
+      valueField.classList.remove('text-control--off');
+    }
+  }
+  else {
+    if (!errorField.classList.contains('errors--off')) {
       errorField.classList.add('errors--off');
     }
 
-    if (!valueField.classList.contains('text-control--off')){
+    if (!valueField.classList.contains('text-control--off')) {
       valueField.classList.add('text-control--off');
     }
   }
 }
 /*******************Function for validate each box********************/
+
+// Call function isValid when form is submit
+form.addEventListener('submit', isValid);
+
 function validateFirstName() {
   const isValid = firstNameField.value !== '' && firstNameField.value.length >= 2;
   updateErrorMessage(firstNameField, errorFirst, isValid);
   return isValid;
 }
-
 
 function validateLastName() {
   const isValid = lastNameField.value !== '' && lastNameField.value.length >= 2;
@@ -98,19 +106,19 @@ function validateEmail() {
 }
 
 function validateBirthDate() {
-  const isValid = birthDateField.value.match(formatDate)!== null;
+  const isValid = birthDateField.value.match(formatDate) !== null;
   updateErrorMessage(birthDateField, errorBirthdate, isValid);
   return isValid;
 }
 
 function validateTournois() {
-  const isValid = tournoisField.value.match(formatNumbers)!== null;
+  const isValid = tournoisField.value.match(formatNumbers) !== null;
   updateErrorMessage(tournoisField, errorTournois, isValid);
   return isValid;
 }
 
 function validateLocation() {
-  const isValid = location1.checked || location2.checked || location3.checked || location4.checked || location5.checked || location6.checked;
+  const isValid = document.querySelectorAll('input[name="location"]:checked').length > 0;
   updateErrorMessage(locationField, errorLocation, isValid);
   return isValid;
 }
@@ -121,15 +129,47 @@ function validateCheckbox() {
   return isValid;
 }
 
-// /*****************Enable or disable the submit button*****************/
-// /**************All boxes must be valid to enable button***************/
+/*****************Enable or disable the submit button*****************/
+/**************All boxes must be valid to enable button***************/
 function isValid() {
-  return !!(
-  validateFirstName()
-  & validateLastName()
-  & validateEmail()
-  & validateBirthDate()
-  & validateTournois()
-  & validateLocation()
-  & validateCheckbox());
+  return (1 === (
+    validateFirstName()
+    & validateLastName()
+    & validateEmail()
+    & validateBirthDate()
+    & validateTournois()
+    & validateLocation()
+    & validateCheckbox()));
+}
+
+/*********************************************************************/
+/****************Display and close validation message*****************/
+/*********************************************************************/
+
+const validationSub = document.getElementById('validation-sub');
+const submitBtn = document.getElementById('subBtn');
+
+// Call function addValidationModal when submiBtn pressed
+submitBtn.addEventListener('click', addValidationModal);
+
+/*************Close field modal and open validation modal*************/
+function addValidationModal() {
+  if (isValid()) {
+    modalbg.classList.remove('bground--visible');
+    validationSub.classList.add('validation-body--vis');
+  }
+}
+
+const closeValid = document.getElementById('close-valid');
+const closeValidBtn = document.getElementById('close-valid-btn');
+
+// Call function closeValidationModal when closeValid or closeValidBtn pressed
+closeValid.addEventListener('click', closeValidationModal);
+closeValidBtn.addEventListener('click', closeValidationModal);
+
+/*************Close validation modal*************/
+function closeValidationModal() {
+  if (validationSub.classList.contains('validation-body--vis')) {
+    validationSub.classList.remove('validation-body--vis');
+  }
 }
